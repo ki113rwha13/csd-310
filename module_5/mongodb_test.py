b6a429e7c149e34364649c23cc60e7a8f02fecff
ki113rwha13 <96151654+ki113rwha13@users.noreply.github.com>
2023-01-15 18:25:49 -0600

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,1 +1,5 @@
-
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.uizza3d.mongodb.net/?retryWrites=true&w=majority";
+client = MongoClient(url)
+db = client.pytech
+print(db.list_collection_names)
